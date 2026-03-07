2b7ea259ad41a216f38c30f805989ae9cb20e24a
Joo Won <110383412+chqkq@users.noreply.github.com>
2026-03-07 18:34:14 +0900

    18258: 큐 문제가 아니었음(큐는 애초에 LinkedList로 만들어졌음). 문제는 System.out을 매번 해줘서임. StringBuilder에 저장하다가, 마지막에 sb를 출력하는 방식으로 하는게 더 빠름.

--- 0x06_큐/18258_큐2/Main.java
+++ 0x06_큐/18258_큐2/Main.java
@@ -8,6 +8,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         // int[] arr = new int[n+1];
         LinkedList<Integer> arr = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
 
         for(int i=0; i<n; i++){
             String s = br.readLine();
@@ -18,34 +19,40 @@ public class Main {
                 arr.addLast(num);
             }
             else if(parts[0].equals("pop")){
-                if(arr.size() == 0) System.out.println(-1);
+                if(arr.size() == 0) sb.append("-1\n");
                 else{
-                    System.out.println(arr.get(0));
+                    sb.append(arr.get(0));
+                    sb.append("\n");
                     arr.removeFirst();
                 }
             }
             else if(parts[0].equals("size")){
-                System.out.println(arr.size());
+                sb.append(arr.size());
+                sb.append("\n");
             }
             else if(parts[0].equals("empty")){
-                if(arr.size() == 0) System.out.println(1);
-                else System.out.println(0);
+                if(arr.size() == 0) sb.append("1\n");
+                else sb.append("0\n");
             }
             else if(parts[0].equals("front")){
-                if(arr.size() == 0) System.out.println(-1);
+                if(arr.size() == 0) sb.append("-1\n");
                 else{
-                    System.out.println(arr.get(0));
+                    sb.append(arr.get(0));
+                    sb.append("\n");
                 }
             }
             else if(parts[0].equals("back")){
-                if(arr.size() == 0) System.out.println(-1);
+                if(arr.size() == 0) sb.append("-1\n");
                 else{
-                    System.out.println(arr.get(arr.size()-1));
+                    sb.append(arr.get(arr.size()-1));
+                    sb.append("\n");
                 }
             }
             // System.out.print(parts[0] + " : ");
             // check(arr);
         }
+        
+        System.out.print(sb);
     }
     
     public static void check(LinkedList<Integer> arr){
