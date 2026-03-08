faae28eefe03e97d7124e21dbc842c29ea423232
Joo Won <110383412+chqkq@users.noreply.github.com>
2026-03-08 17:32:51 +0900

    10799: 시간초과의 원인이 LinkedList인 것 같고, 이 문제를 굳이 LinkedList로 풀 필요 없이 그냥 arr로도 할 수 있을 것 같아서 수정함

--- 0x08_스택의활용/10799_쇠막대기/Main.java
+++ 0x08_스택의활용/10799_쇠막대기/Main.java
@@ -7,26 +7,32 @@ public class Main {
     public static void main(String[] args) throws Exception{
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
        String str = br.readLine();
-       LinkedList<Integer> list = new LinkedList<>();
+       int[] list = new int[str.length()];
+       int end = 0;
+    //    LinkedList<Integer> list = new LinkedList<>();
        int answer = 0;
 
        for(int i=0; i<str.length(); i++){
             if(str.charAt(i) == '('){
-                list.addLast(0);
+                list[end] = 0;
+                end++;
             }
             else{
                 // 레이저인 경우
-                int check = list.get(list.size()-1);
+                int size = end;
+                int check = list[end-1];
                 if( check == 0){
-                    list.removeLast();
-                    for(int j=0; j<list.size(); j++){
-                        list.set(j, list.get(j)+1);
+                    list[end-1] = 0;
+                    end--;
+                    for(int j=0; j<size; j++){
+                        list[j]++;
                     }
                 }
                 // 막대인 경우
                 else{
                     answer += (check + 1);
-                    list.removeLast();
+                    list[end-1] = 0;
+                    end--;
                 }
             }
        }
