442daf398c4db978a2f555e2b60d3b0d0070fa9d
Joo Won <110383412+chqkq@users.noreply.github.com>
2026-03-12 23:18:42 +0900

    4179: 로직 전면 수정

--- 0x09_BFS/4179_불!/Main.java
+++ 0x09_BFS/4179_불!/Main.java
@@ -8,10 +8,11 @@ public class Main {
         int row = Integer.parseInt(s.split(" ")[0]);
         int col = Integer.parseInt(s.split(" ")[1]);
         char[][] arr = new char[row][col];
-        boolean[][] visited_j = new boolean[row][col];
-        boolean[][] visited_f = new boolean[row][col];
+        int[][] jihoon = new int[row][col];
+        int[][] fire = new int[row][col];
         Queue<int[]> fQ = new LinkedList<>();
         Queue<int[]> jQ = new LinkedList<>();
+        LinkedList<int[]> list = new LinkedList<>();
 
         for(int i=0; i<row; i++){
             s = br.readLine();
@@ -19,80 +20,91 @@ public class Main {
                 arr[i][j] = s.charAt(j);
                 if(arr[i][j] == 'J'){
                     jQ.offer(new int[]{i,j});
+                    jihoon[i][j] = 1;
                 } 
                 else if(arr[i][j] == 'F'){
                     fQ.offer(new int[]{i,j});
+                    fire[i][j] = 1;
                 } 
+                else if(arr[i][j] == '#'){
+                    jihoon[i][j] = -1;
+                    fire[i][j] = -1;
+                }
+
+                if(i==0 && arr[i][j] != '#'){
+                    list.add(new int[]{i,j});
+                }
+                if(j==0 && arr[i][j] != '#'){
+                    list.add(new int[]{i,j});
+                }
+                if(j==col-1 && arr[i][j] != '#'){
+                    list.add(new int[]{i,j});
+                }
+                if(i==row-1 && arr[i][j] != '#'){
+                    list.add(new int[]{i,j});
+                }
             }
         }
+
 
         // 상 하 좌 우
         int[] dr = new int[]{1, -1, 0, 0};
         int[] dc = new int[]{0, 0, -1, 1};
-        boolean finish = true;
-        int checkJ=0; int checkF=0;
-        int nextJ=1; int nextF=1;
-        int answer = 0;
 
-        while(finish){
-            // for(int i=0; i<row; i++){
-            //     for(int j=0; j<col; j++){
-            //         System.out.print(arr[i][j] + " ");
-            //     }
-            //     System.out.println();
-            // }
-            // System.out.println();
-            answer++;
-            checkJ = 0;
-            for(int j=0; j<nextJ; j++){
-                int[] jNow = jQ.poll();
-                if(arr[jNow[0]][jNow[1]] == 'F') continue;
-                for(int i=0; i<4; i++){
-                    int jNew_r = jNow[0] + dr[i];
-                    int jNew_c = jNow[1] + dc[i];
-                    if(visited_j[jNow[0]][jNow[1]] == true) continue;
-                    if(jNew_r<0 || jNew_r>=row || jNew_c<0 || jNew_c>=col) continue;
-                    if(arr[jNew_r][jNew_c] == 'F' || arr[jNew_r][jNew_c] == '#') continue;
+        while(!jQ.isEmpty()){
+            int[] now = jQ.poll();
+            for(int i=0; i<4; i++){
+                int newR = now[0] + dr[i];
+                int newC = now[1] + dc[i];
 
-                    jQ.offer(new int[]{jNew_r,jNew_c});
-                    checkJ++;
-                    arr[jNow[0]][jNow[1]] = '.';
-                    arr[jNew_r][jNew_c] = 'J';
-                }
-                visited_j[jNow[0]][jNow[1]] = true;
+                if(newR < 0 || newR >= row || newC < 0 || newC >= col) continue;
+                if(jihoon[newR][newC] >= 1 || jihoon[newR][newC] == -1) continue;
+
+                jihoon[newR][newC] = jihoon[now[0]][now[1]]+1;
+                jQ.offer(new int[]{newR, newC});
             }
-            nextJ = checkJ;
-            checkF = 0;
-            for(int j=0; j<nextF; j++){
-                int[] fNow = fQ.poll();
-                for(int i=0; i<4; i++){
-                    int fNew_r = fNow[0] + dr[i];
-                    int fNew_c = fNow[1] + dc[i];
-                    if(visited_f[fNow[0]][fNow[1]] == true) continue;
-                    if(fNew_r<0 || fNew_r>=row || fNew_c<0 || fNew_c>=col) continue;
-                    if(arr[fNew_r][fNew_c] == 'F' || arr[fNew_r][fNew_c] == '#') continue;
-
-                    fQ.offer(new int[]{fNew_r,fNew_c});
-                    checkF++;
-                    arr[fNew_r][fNew_c] = 'F';
-                }
-                visited_f[fNow[0]][fNow[1]] = true;
-            }
-            nextF = checkF;
-            if(jQ.isEmpty()) finish = false;
-
-            // for(int i=0; i<row; i++){
-            //     for(int j=0; j<col; j++){
-            //         System.out.print(arr[i][j] + " ");
-            //     }
-            //     System.out.println();
-            // }
-            // System.out.println();
-            // System.out.println("*****");
 
         }
 
-        System.out.print(answer);
+        while(!fQ.isEmpty()){
+            int[] now = fQ.poll();
+            for(int i=0; i<4; i++){
+                int newR = now[0] + dr[i];
+                int newC = now[1] + dc[i];
+
+                if(newR < 0 || newR >= row || newC < 0 || newC >= col) continue;
+                if(fire[newR][newC] >= 1 || fire[newR][newC] == -1) continue;
+
+                fire[newR][newC] = fire[now[0]][now[1]]+1;
+                fQ.offer(new int[]{newR, newC});
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(fire[i][j] == -1) continue;
+                if(fire[i][j] != 0 && fire[i][j] <= jihoon[i][j]) arr[i][j] = 'F';
+                else arr[i][j] = 'J';
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+
+        for(int[] edge : list){
+            int r = edge[0];
+            int c = edge[1];
+
+            if(jihoon[r][c] == 0) continue;
+
+            if(fire[r][c] == 0 || jihoon[r][c] < fire[r][c]){
+                answer = Math.min(answer, jihoon[r][c]);
+            }
+        }
+        if(answer == Integer.MAX_VALUE){
+            System.out.println("IMPOSSIBLE");
+        }else{
+            System.out.println(answer);
+        }
 
     }
 }
