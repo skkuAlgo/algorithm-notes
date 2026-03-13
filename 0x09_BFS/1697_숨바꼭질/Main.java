8e2b6184e512749adb4cfcbadc884e900c7d0a1c
Joo Won <110383412+chqkq@users.noreply.github.com>
2026-03-13 22:55:51 +0900

    1697: 다 채우고 마지막에 확인

--- 0x09_BFS/1697_숨바꼭질/Main.java
+++ 0x09_BFS/1697_숨바꼭질/Main.java
@@ -5,36 +5,39 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
+        sc.close();
 
-        int[] arr = new int[100000];
+        int[] arr = new int[100001];
         Queue<Integer> q = new LinkedList<>(); 
         q.offer(N);
         arr[N] = 1;
         int answer = 0;
 
-        while(arr[K] == 0){
+        while(!q.isEmpty()){
             int num = q.poll();
             int c = arr[num];
-            if(num-1 == N || num+1 == N || num*2 == N){
-                answer = c+1;
-                break;
-            }
+            // if(num-1 == N || num+1 == N || num*2 == N){
+            //     answer = c+1;
+            //     break;
+            // }
             
-            if(num-1 >= 0){
+            if(num-1 >= 0 && arr[num-1] == 0){
                 arr[num-1] = c+1;
                 q.offer(num-1);
             } 
-            if(num+1 <= 100000){
+            if(num+1 <= 100000 && arr[num+1] == 0){
                arr[num+1] = c+1; 
                q.offer(num+1);
             } 
-            if(num*2 <= 100000){
+            if(num*2 <= 100000 && arr[num*2] == 0){
                arr[num*2] = c+1; 
                q.offer(num*2);
             } 
+
         }
 
-        System.out.print(answer+1);
+        // System.out.print(answer+1);
+        System.out.print(arr[K]-1);
          
     }
 }
