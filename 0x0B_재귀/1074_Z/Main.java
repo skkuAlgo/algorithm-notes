a3cae6f1f68b41319134b8b32e227b663fb5f96b
Joo Won <110383412+chqkq@users.noreply.github.com>
2026-03-23 20:38:23 +0900

    1074: 애초에 배열 선언하는게 메모리를 많이 잡아먹음. 해당 위치에 올 때 까지의 값을 더하는 식으로 해결

--- 0x0B_재귀/1074_Z/Main.java
+++ 0x0B_재귀/1074_Z/Main.java
@@ -17,8 +17,9 @@ public class Main {
         }
 
         arr = new int[len][len];
+        Z(0,0,len,r,c,0);
 
-        Z(0,0,len);
+        // Z(0,0,len);
 
         // for(int i=0; i<len; i++){
         //     for(int j=0; j<len; j++){
@@ -26,26 +27,60 @@ public class Main {
         //     }
         //     System.out.println();
         // }
-
-        System.out.print(arr[r][c]);
+        
+        
     }
 
     public static int check = 0;
 
-    public static void Z(int x, int y, int r){
-        if(r == 2){
-            arr[x][y] = check*4 + 0;
-            arr[x][y+1] = check*4 + 1;
-            arr[x+1][y] = check*4 + 2;
-            arr[x+1][y+1] = check*4 + 3;
-            check++;
+    // public static void Z(int x, int y, int r){
+    //     if(r == 2){
+    //         arr[x][y] = check*4 + 0;
+    //         arr[x][y+1] = check*4 + 1;
+    //         arr[x+1][y] = check*4 + 2;
+    //         arr[x+1][y+1] = check*4 + 3;
+    //         check++;
+    //     }
+    //     else{
+    //         Z(x, y, r/2);
+    //         Z(x, y+r/2, r/2);
+    //         Z(x+r/2, y, r/2);
+    //         Z(x+r/2, y+r/2, r/2);
+    //     }
+    // }
+
+    public static void Z(int row, int col, int len, int x, int y, int answ){
+        if(len == 2){
+            if(x == row && y == col){
+                System.out.print(answ);
+            }
+            else if(x == row && y == col+1){
+                System.out.print(answ+1);
+            }
+            else if(x == row+1 && y == col){
+                System.out.print(answ+2);
+            }
+            else if(x == row+1 && y == col+1){
+                System.out.print(answ+3);
+            }
         }
         else{
-            Z(x, y, r/2);
-            Z(x, y+r/2, r/2);
-            Z(x+r/2, y, r/2);
-            Z(x+r/2, y+r/2, r/2);
+            int multi = len*len/4;
+        
+            if(x < row+(len/2) && y < (col+(len/2))){
+                Z(row, col, len/2, x, y, answ);
+            }
+            else if(x < row+(len/2) && y >= (col+(len/2))){
+                Z(row, col+(len/2), len/2, x, y, answ + multi);
+            }
+            else if(x >= row+(len/2) && y < (col+(len/2))){
+                Z(row+(len/2), col, len/2, x, y, answ + (multi*2));
+            }
+            else if(x >= row+(len/2) && y >= (col+(len/2))){
+                Z(row+(len/2), col+(len/2), len/2, x, y, answ + (multi*3));
+            }
         }
+
     }
 }
 
